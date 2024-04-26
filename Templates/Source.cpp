8a2dc5cb1d1842c8d4f4a49a8fd75c685a730d66
noobsln <alyssamyers2020@gmail.com>
2024-04-26 02:15:16 -0400

    driver created

--- Templates/Source.cpp
+++ Templates/Source.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 template<typename T> T half(T num) { return num / 2; }
 template<> int half<int>(int num) { return static_cast<int>(num)/2; }
+
+int main() {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << '\n' << half(b) << '\n' << half(c) << '\n';
+	return 0;
+}
